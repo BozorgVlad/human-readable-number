274aea3055cc93badad7cef09fdd98a54b8e733d
BozorgVlad <bozorgvlad@gmail.com>
2020-03-01 22:14:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
+ module.exports = function toReadable (number) {
+  let simpNumb = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+                   'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let compNumb = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let simpNum = Math.floor(number / 10) - 2;
+  let remainder = number % 10;
+  let compNum = Math.floor(number / 100);
+  let rem = number % 100;
+  let strRem = String(rem);
+  let arrRem = strRem.split('');
+  if (number < 20) {
+      return simpNumb[number];
+  } else if (number % 10 == 0 && number < 100) {
+      return compNumb[simpNum];
+  } else if(number == 100) {
+      return 'one hundred';
+  } else if (number > 20 && number < 100) {
+    return compNumb[simpNum] + " " + simpNumb[remainder];
+  } else if (number % 100 == 0 && number >= 100) {
+      return simpNumb[compNum] + ' hundred';
+  } else if (rem < 20) {
+      return simpNumb[compNum] + ' hundred ' + simpNumb[rem];
+  } else if (rem % 10 == 0) {
+    return simpNumb[compNum] + ' hundred ' + compNumb[rem / 10 - 2]
+  } else if (rem > 20) {
+      return simpNumb[compNum] + ' hundred ' + compNumb[arrRem[0] - 2] + ' ' + simpNumb[rem % 10];
 }
+ }
